e6fd73f7972fc92a3a5e4a343b50280918ff7e16
Kirill Kogut <kirilrempov@gmail.com>
2021-06-29 13:11:39 +0300

    directional light

---
+++ shaders/cube-fs.glsl
@@ -0,0 +1,48 @@
+#version 330 core
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float shininess;
+    sampler2D emissionMap;
+};
+
+struct Light {
+    // vec3 position;
+    vec3 direction;
+    
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+out vec4 FragColor;
+
+in vec3 Normal;
+in vec3 FragPos;
+in vec2 TexPos;
+  
+uniform vec3 lightColor;
+uniform vec3 objectColor;
+uniform Material material;
+uniform Light light;
+
+void main()
+{
+    // ambient
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexPos));
+      
+    // diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(-light.direction);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexPos).rgb;
+    
+    vec3 viewDir = normalize(-FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * spec * texture(material.specular, TexPos).rgb;
+            
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
+}
